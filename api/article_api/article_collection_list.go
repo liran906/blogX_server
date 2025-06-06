a150907dd5ae276c988987cb7cece9c7b6f70c45
liran906 <liran900620@gmail.com>
2025-06-06 23:22:35 +0800

    small refactors

--- api/article_api/article_collection_list.go
+++ api/article_api/article_collection_list.go
@@ -118,7 +118,7 @@ func (ArticleApi) ArticleCollectionListView(c *gin.Context) {
 	for _, article := range _list {
 		article.Content = ""                              // 正文在 list 中不返回
 		_ = redis_article.GetAllTypesForArticle(&article) // 读取缓存中的数据
-		data := ArticleListResp{ // 响应结构体
+		data := ArticleListResp{                          // 响应结构体
 			ArticleModel:  article,
 			UserNickname:  article.UserModel.Nickname,
 			UserAvatarURL: article.UserModel.AvatarURL,
